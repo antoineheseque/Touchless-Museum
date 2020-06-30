f06cd91417fef6e054a32197ad1a1b14b08dc8a2
Antoine Heseque <hesequeantoine@gmail.com>
2020-06-30 21:18:38 +0200

    Peinture n1 Statue n1 Hall principal Systeme de main

---
+++ Touchless-Museum/Assets/Project/Scripts/SwitchSceneButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSceneButton : MonoBehaviour
+{
+    public void OnClickButton()
+    {
+        Debug.Log("Button clicked!");
+    }
+}
