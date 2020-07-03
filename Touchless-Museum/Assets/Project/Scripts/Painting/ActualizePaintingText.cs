b12da99a3fe3f7d01623feeab4f98d4dac20a53e
Antoine Heseque <hesequeantoine@gmail.com>
2020-07-04 00:03:45 +0200

    2 new paintings Painting switch buttons New tutorial video

---
+++ Touchless-Museum/Assets/Project/Scripts/Painting/ActualizePaintingText.cs
@@ -0,0 +1,150 @@
+﻿
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ActualizePaintingText : MonoBehaviour
+{
+    private const float REMOVE_TIME = .5f;
+    private const float SHOW_TIME = 2f;
+
+    [SerializeField] private TMP_Text title = null,
+                                      author = null, 
+                                      creationDate = null,
+                                      description = null;
+
+    private Coroutine showTextCoroutine = null;
+
+    [SerializeField] private PaintingScriptableObject paint = null;
+    
+    private void Start()
+    {
+        ChangeText(paint);
+    }
+    
+    /// <summary>
+    /// Change text details of a painting
+    /// </summary>
+    /// <param name="painting"></param>
+    public void ChangeText(PaintingScriptableObject painting)
+    {
+        if (showTextCoroutine != null)
+        {
+            StopAllCoroutines(); // Make sure the remove coroutines are stopped, including the showTextCoroutine
+        }
+            
+        showTextCoroutine = StartCoroutine(ChangeTextCoroutine(painting));
+    }
+
+    /// <summary>
+    /// Remove all lines at the same time
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator RemoveAllText()
+    {
+        int maxCharacter = Mathf.Max(Mathf.Max(Mathf.Max(title.text.Length, author.text.Length), creationDate.text.Length), description.text.Length);
+        float timePerCharacter = REMOVE_TIME / maxCharacter;
+        float timer = timePerCharacter;
+        int rdmChar = 0;
+        
+        while (maxCharacter > 0)
+        {
+            while (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                rdmChar = (rdmChar+1)%14;
+                
+                if (title.text.Length > 0)
+                    title.text = title.text.Remove(title.text.Length - 1) + (char)(rdmChar+32);
+                if (author.text.Length > 0)
+                    author.text = author.text.Remove(author.text.Length - 1) + (char)(rdmChar+32);
+                if (creationDate.text.Length > 0)
+                    creationDate.text = creationDate.text.Remove(creationDate.text.Length - 1) + (char)(rdmChar+32);
+                if (description.text.Length > 0)
+                    description.text = description.text.Remove(description.text.Length - 1) + (char)(rdmChar+32);
+                yield return null;
+            }
+
+            timer = timePerCharacter;
+            maxCharacter -= 1;
+            if (title.text.Length > 0)
+                title.text = title.text.Remove(title.text.Length - 1);
+            if (author.text.Length > 0)
+                author.text = author.text.Remove(author.text.Length - 1);
+            if (creationDate.text.Length > 0)
+                creationDate.text = creationDate.text.Remove(creationDate.text.Length - 1);
+            if (description.text.Length > 0)
+                description.text = description.text.Remove(description.text.Length - 1);
+        }
+
+        yield return true;
+    }
+
+    private IEnumerator ChangeTextCoroutine(PaintingScriptableObject painting)
+    {
+        yield return StartCoroutine(RemoveAllText());
+        
+        // Show text line by line
+        int characterCount = painting.paintingName.Length + painting.author.Length + painting.author.Length + painting.description.Length;
+        int currentCharacter = 0;
+        float timePerCharacter = SHOW_TIME / characterCount;
+        float timer = timePerCharacter;
+        
+        while (currentCharacter < characterCount)
+        {
+            while (timer > 0)
+                timer -= Time.deltaTime;
+
+            timer = timePerCharacter;
+            
+            // Add character to the right text
+            TMP_Text selected = TextToSelect(painting, currentCharacter, out char character);
+            if(selected) selected.text += character;
+            
+            currentCharacter++;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// TODO: Not sure if this is the best way?
+    /// Select the right text and index from text to add the right char
+    /// </summary>
+    /// <param name="painting"></param>
+    /// <param name="currentCharacter"></param>
+    /// <param name="character"></param>
+    /// <returns></returns>
+    private TMP_Text TextToSelect(PaintingScriptableObject painting, int currentCharacter, out char character)
+    {
+        int index = currentCharacter;
+        if (index < painting.paintingName.Length)
+        {
+            character = painting.paintingName[index];
+            return title;
+        }
+        index -= painting.paintingName.Length;
+
+        if (index < painting.author.Length)
+        {
+            character = painting.author[index];
+            return author;
+        }
+        index -= painting.author.Length;
+
+        if (index < painting.creationDate.Length)
+        {
+            character = painting.creationDate[index];
+            return creationDate;
+        }
+        index -= painting.creationDate.Length;
+        
+        if (index < painting.description.Length)
+        {
+            character = painting.description[index];
+            return description;
+        }
+        
+        character = ' ';
+        return null;
+    }
+}
---
+++ Touchless-Museum/Assets/Project/Scripts/Painting/Paintings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Leap;
+using Leap.Unity;
+using UnityEditor;
+using UnityEngine;
+
+public class Paintings : MonoBehaviour
+{
+    [SerializeField] private ActualizePaintingText actualizer = null;
+    [SerializeField] private PaintingScriptableObject[] paintings = null;
+    [SerializeField] private Transform paintingAnchor = null;
+
+    
+    private int currentIndex = 0;
+    private LeapProvider leapProvider;
+
+    private void Awake()
+    {
+        leapProvider = FindObjectOfType<LeapProvider>();
+    }
+
+    private void Start()
+    {
+        ShowPainting(currentIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ShowPainting((currentIndex+1)%paintings.Length);
+        }
+    }
+
+    private void ShowPainting(int index)
+    {
+        RemovePainting();
+
+        // Add painting and change text details
+        actualizer.ChangeText(paintings[index]);
+
+        GameObject painting = Instantiate(paintings[index].gameObject, paintingAnchor, false);
+        currentIndex = index;
+    }
+
+    public void SwitchLeft()
+    {
+        int index = currentIndex-1;
+        if (index < 0) index = paintings.Length-1;
+        ShowPainting(index);
+    }
+
+    public void SwitchRight()
+    {
+        ShowPainting((currentIndex+1)%paintings.Length);
+    }
+
+    private void RemovePainting()
+    {
+        if (paintingAnchor.childCount > 0)
+        {
+            Destroy(paintingAnchor.GetChild(0).gameObject);
+        }
+    }
+}
