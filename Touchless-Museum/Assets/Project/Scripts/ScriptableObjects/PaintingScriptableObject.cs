28afc27c56fa2134ebcd0c8ca7428ab07616cc20
Antoine Heseque <hesequeantoine@gmail.com>
2020-07-03 16:34:23 +0200

    Painting text + 3D Model Lighting

---
+++ Touchless-Museum/Assets/Project/Scripts/ScriptableObjects/PaintingScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Painting", menuName = "ScriptableObjects/PaintingScriptableObject")]
+public class PaintingScriptableObject : ScriptableObject
+{
+    public string paintingName = "Unknown";
+    public GameObject gameObject;
+    public string author = "Antoine";
+    public string creationDate = "2020";
+    public string description = "";
+}
--- Touchless-Museum/Assets/Project/Scripts/SwitchSceneButton.cs
+++ Touchless-Museum/Assets/Project/Scripts/SwitchSceneButton.cs
@@ -4,9 +4,15 @@ public class SwitchSceneButton : MonoBehaviour
 {
     [SerializeField] private string levelName = "Main";
     [SerializeField] private Animator anim = null;
+    
+    private static readonly int Loading = Animator.StringToHash("Loading");
+
     public void OnClickButton()
     {
-        anim.SetTrigger(levelName);
+        if(anim)
+            anim.SetTrigger(Loading);
+        else
+            Debug.LogError("An animator is missing.");
         SceneLoader.LoadScene(levelName);
     }
 }
